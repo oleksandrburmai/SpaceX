fba1ff3861046096a881715f21ff3387e83199db
oleksandrburmai <alex.burmai@yahoo.com>
2018-03-23 10:39:27 +0200

    dev: Delete variables in method engineFuelCapacity.

--- src/rocket/Engine.java
+++ src/rocket/Engine.java
@@ -5,9 +5,6 @@ import actionWhithFuel.FuelCapacity;
 public class Engine implements FuelCapacity {
 
     public int engineFuelCapacity() {
-        int degreeOneFuelCapacity = fuelCapacity();
-        int degreeTwoFuelCapacity = fuelCapacity();
-        int degreeThreeFuelCapacity = fuelCapacity();
-        return degreeOneFuelCapacity + degreeTwoFuelCapacity + degreeThreeFuelCapacity;
+        return fuelCapacity() + fuelCapacity() + fuelCapacity();
     }
 }
