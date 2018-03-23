608eb79cb98853b414297ea3a2148c87f47d160b
oleksandrburmai <alex.burmai@yahoo.com>
2018-03-23 02:49:48 +0200

    dev: Renamed use method.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new SpaceX().isSuccessfulFly(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]));
+        System.out.println(new SpaceX().fly(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]));
     }
 }
