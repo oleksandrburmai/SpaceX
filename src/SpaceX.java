2e2afaec8b9dbf200176137986428f9dbefc1d33
oleksandrburmai <alex.burmai@yahoo.com>
2018-03-25 16:37:52 +0300

    dev: some change.

--- src/SpaceX.java
+++ src/SpaceX.java
@@ -31,9 +31,9 @@ public class SpaceX implements FlyDelegate {
 
         if (rocket.totalFuelCapacity() > planet.getDistance() * rocket.fuelConsumption() / 1000) {
             double time = rocket.rocketAccelerationTill2SpaceSpeed() +
-                    (planet.getDistance() - Math.pow(rocket.rocketAccelerationTill2SpaceSpeed(), 2) / 2) / 40020;
+                    (planet.getDistance() - Math.pow(rocket.rocketAccelerationTill2SpaceSpeed(), 2) / 2.0) / 40020.0;
             return "Your fly to planet " + planet.getPlanetName() + " is successful\n" +
-                    "And you will spend " + (Math.round(time / 60)) + " minutes";
+                    "And you will spend " + (Math.round(time / 60.0)) + " minutes";
         } else {
             return "Your fly to planet " + planet.getPlanetName() + " is not successful";
         }
