6ae2e8fbf49552e64f4653a0507d5fc60cc2ad16
oleksandrburmai <alex.burmai@yahoo.com>
2018-03-27 18:09:54 +0300

    dev: some change.

---
+++ src/rocket/parts/cabin/Cabin.java
@@ -0,0 +1,43 @@
+package rocket.parts.cabin;
+
+public class Cabin implements CabinProvider {
+
+    private int weight;
+    private double capacity;
+    private String cabinName;
+    private int height;
+    private int width;
+
+    public Cabin(Cabins cabin) {
+        this.cabinName = cabin.getCabinName();
+        this.weight = cabin.getWeight();
+        this.capacity = cabin.getCapacity();
+        this.height = cabin.getHeight();
+        this.width = cabin.getWidth();
+    }
+
+    @Override
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public double getCapacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public String getCabinName() {
+        return this.cabinName;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.width;
+    }
+}
